1bc76b14f8503c33166bed3ff2efe5f981677dde
Eugeny <evchepurnykh@gmail.com>
2022-06-16 20:32:42 +1000

    Сode simplified. Removed try-catch.

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -7,26 +7,22 @@ public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
 
-    public T poll() {
-        T first = out.pop();
-        if (Objects.isNull(first)) {
-            throw new NoSuchElementException();
+    private int count = 0;
+
+    public T poll() throws NoSuchElementException {
+        for (int i = 0; i < count; i++) {
+            out.push(in.pop());
         }
-        return first;
+        T value = out.pop();
+        count--;
+        for (int i = 0; i < count; i++) {
+            in.push(out.pop());
+        }
+        return value;
     }
 
     public void push(T value) {
-        T val;
-        try {
-            while ((val = out.pop()) != null) {
-                in.push(val);
-            }
-        } catch (NoSuchElementException e) { }
         in.push(value);
-        try {
-            while ((val = in.pop()) != null) {
-                out.push(val);
-            }
-        } catch (NoSuchElementException e) { }
+        count++;
     }
 }
