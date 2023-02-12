717d959261b00205a470ed47eeefa74bf6aae811
Eugeny_ch <evch@rambler.ru>
2023-02-12 12:56:34 +1000

    Add Box and tests.

---
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -0,0 +1,81 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BoxTest {
+    @Test
+    void isThisSphere() {
+        Box box = new Box(0, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Sphere");
+    }
+
+    @Test
+    void isThisCube() {
+        Box box = new Box(8, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Cube");
+    }
+
+    @Test
+    void tetrahedronHas4Vertices() {
+        Box box = new Box(4, 5);
+        int vertices = box.getNumberOfVertices();
+        assertThat(vertices).isEqualTo(4);
+    }
+
+    @Test
+    void sphereHas0Vertices() {
+        Box box = new Box(0, 5);
+        int vertices = box.getNumberOfVertices();
+        assertThat(vertices).isEqualTo(0);
+    }
+
+    @Test
+    void verticesIsUNKNOWNIsEqualsToMinus1() {
+        Box box = new Box(35, 5);
+        int vertices = box.getNumberOfVertices();
+        assertThat(vertices).isEqualTo(-1);
+        String type = box.whatsThis();
+        assertThat(type).isEqualTo("Unknown object");
+    }
+
+    @Test
+    void verticesIsEqualToMinus1AndTypeQualsToUnknownIfEdgeLessThan1() {
+        Box box = new Box(4, 0);
+        int vertices = box.getNumberOfVertices();
+        assertThat(vertices).isEqualTo(-1);
+        String type = box.whatsThis();
+        assertThat(type).isEqualTo("Unknown object");
+    }
+
+    @Test
+    void BoxIsKnown() {
+        Box box = new Box(4, 1);
+        boolean known = box.isExist();
+        assertThat(known).isTrue();
+    }
+
+    @Test
+    void BoxIsUnknown() {
+        Box box = new Box(4, -1);
+        boolean known = box.isExist();
+        assertThat(known).isFalse();
+    }
+
+    @Test
+    void squareOfSphereIsAbout1256() {
+        Box box = new Box(0, 1);
+        double square = box.getArea();
+        assertThat(((Double)(square)).toString().contains("12.56")).isTrue();
+    }
+
+    @Test
+    void squareOfCubeIs24() {
+        Box box = new Box(8, 2);
+        double square = box.getArea();
+        assertThat(square).isEqualTo(24.0);
+    }
+}
