148aee1f50911582934a2cdffa3d9abcbd461f30
Eugeny_ch <evch@rambler.ru>
2023-03-16 19:35:56 +1000

    3. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,48 @@
+package ru.job4j.question;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Analize {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+        int added;
+        int changed;
+        int deleted;
+        Set<User> temporarySet = new HashSet<>();
+        Set<Integer> temporarySetIds = new HashSet<>();
+
+        Set<Integer> previousIds = new HashSet<>();
+        for (User user : previous) {
+            previousIds.add(user.getId());
+        }
+        Set<Integer> currentIds = new HashSet<>();
+        for (User user : current) {
+            currentIds.add(user.getId());
+        }
+
+        //calculate added
+        temporarySetIds.addAll(previousIds);
+        temporarySetIds.addAll(currentIds);
+        added = temporarySetIds.size() - previousIds.size();
+
+        //calculate deleted
+        temporarySetIds.clear();
+        temporarySetIds.addAll(currentIds);
+        temporarySetIds.addAll(previousIds);
+        deleted = temporarySetIds.size() - currentIds.size();
+
+        //calculate changed
+        temporarySet.addAll(previous);
+        temporarySet.retainAll(current);
+        temporarySetIds.clear();
+        temporarySetIds.addAll(previousIds);
+        temporarySetIds.retainAll(currentIds);
+        changed = temporarySetIds.size() - temporarySet.size();
+
+        return new Info(added, changed, deleted);
+    }
+
+}
