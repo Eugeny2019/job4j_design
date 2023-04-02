552fbd5acb2dc57907bfc67a632279f3e1a22cf0
Eugeny_ch <evch@rambler.ru>
2023-04-02 17:11:00 +1000

    Exception -> IOException

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -1,6 +1,7 @@
-package ru.job4j;
+package ru.job4j.io;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
@@ -15,7 +16,7 @@ public class EvenNumberFile {
                 int evenDefined = Integer.parseInt(line);
                 System.out.println(line + (evenDefined % 2 == 0 ? " - четное" : " - нечетное"));
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
