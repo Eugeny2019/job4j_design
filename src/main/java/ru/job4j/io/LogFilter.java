7deaa10cf4e24f7a250bd2d1ee36fa4e766af88c
Eugeny_ch <evch@rambler.ru>
2023-04-04 01:10:43 +1000

    delete 1 collection, use predicate, const.equals(var). Print without []

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -5,29 +5,28 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class LogFilter {
     public List<String> filter(String file) {
-        List<String> resultLines = new ArrayList<>();
         List<String> lines = new ArrayList<>();
+
+        Predicate<String> predicate = s -> {
+            String[] tempList = s.split(" ");
+            return tempList.length > 1 && "404".equals(tempList[tempList.length - 2]);
+        };
+
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            lines = new ArrayList<>(in.lines().toList());
+            lines = new ArrayList<>(in.lines().filter(predicate).toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        for (String line : lines) {
-            String[] tempList = line.split(" ");
-            if (tempList.length > 1 && tempList[tempList.length - 2].equals("404")) {
-                resultLines.add(line);
-            }
-        }
-        return resultLines;
+        return lines;
     }
 
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("data/log.txt");
-        System.out.println(log);
-
+        log.forEach(System.out::println);
     }
 }
