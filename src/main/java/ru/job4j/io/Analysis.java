0903f519bb5deeac454d22c99d918f56a4a20c81
Eugeny_ch <evch@rambler.ru>
2023-04-15 09:14:22 +1000

    decrease number of ifs to one

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -3,20 +3,17 @@ package ru.job4j.io;
 import java.io.*;
 
 public class Analysis {
-    @SuppressWarnings("checkstyle:InnerAssignment")
+
     public void unavailable(String source, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
              BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
             String line;
             boolean isError = false;
             while ((line = reader.readLine()) != null) {
-                if (line.matches("[400|500].*") && !isError) {
-                    isError = true;
+                if (line.matches("[45]0{2}\s.*") && !isError || !line.matches("[45]0{2}\s.*") && isError) {
+                    isError = !isError;
                     writer.write(line.split(" ")[1] + ";");
-                }
-                if (line.matches("[200|300].*") && isError) {
-                    isError = false;
-                    writer.write(line.split(" ")[1] + ";" + System.lineSeparator());
+                    writer.write((!isError) ? System.lineSeparator() : "");
                 }
             }
 
