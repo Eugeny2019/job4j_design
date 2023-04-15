bd80391f3e3b1ce181359a75eb49a4a2348c694f
Eugeny_ch <evch@rambler.ru>
2023-04-15 22:36:16 +1000

    decrease number of ifs to one (next variant, prompted)

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -8,12 +8,14 @@ public class Analysis {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
              BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
             String line;
-            boolean isError = false;
+            boolean isError = true;
             while ((line = reader.readLine()) != null) {
-                if (line.matches("[45]0{2}\s.*") && !isError || !line.matches("[45]0{2}\s.*") && isError) {
+                if ((line.startsWith("400") || line.startsWith("500")) == isError
+                        || !(line.startsWith("400") || line.startsWith("500")) == !isError) {
                     isError = !isError;
-                    writer.write(line.split(" ")[1] + ";");
-                    writer.write((!isError) ? System.lineSeparator() : "");
+                    writer.append(line.split(" ")[1]);
+                    writer.append(";");
+                    writer.append((isError) ? System.lineSeparator() : "");
                 }
             }
 
