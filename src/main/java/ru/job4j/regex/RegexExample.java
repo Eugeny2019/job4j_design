cede82f3efde99fcb5bbb7c52f4410e3c2eb97b2
Eugeny_ch <evch@rambler.ru>
2023-04-29 20:10:46 +1000

    15.1. Регулярные выражения. Метасимволы, квантификаторы, классы символов [#504918]

--- src/main/java/ru/job4j/regex/RegexExample.java
+++ src/main/java/ru/job4j/regex/RegexExample.java
@@ -5,16 +5,11 @@ import java.util.regex.Pattern;
 
 public class RegexExample {
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("Я учусь на Job4j");
-
-        String text1 = "Я учусь на Job4j";
-        Matcher matcher1 = pattern.matcher(text1);
-        boolean isPresent1 = matcher1.matches();
-        System.out.println(isPresent1);
-
-        String text2 = "Я учусь на курсе Job4j";
-        Matcher matcher2 = pattern.matcher(text2);
-        boolean isPresent2 = matcher2.matches();
-        System.out.println(isPresent2);
+        Pattern pattern = Pattern.compile("\\b\\S+\\S+\\.\\S+\\b");
+        String text = "peter-2022@example.com example65@mail_box.ru 123_45@example-mailbox.com";
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println("Найдено совпадение: " + matcher.group());
+        }
     }
 }
