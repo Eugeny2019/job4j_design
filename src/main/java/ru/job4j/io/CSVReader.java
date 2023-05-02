85b6c4a4b05b05c00a562be24faadd325868393a
Eugeny_ch <evch@rambler.ru>
2023-05-02 22:58:48 +1000

    17. Scanner [#504791]

---
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -0,0 +1,86 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.*;
+
+
+public class CSVReader {
+    public static void handle(ArgsName argsName) throws Exception {
+        try (Scanner sc = new Scanner(new FileInputStream(argsName.get("path")))) {
+            sc.useDelimiter(System.lineSeparator());
+            if (!sc.hasNextLine()) {
+                throw new NoSuchElementException(String.format("There is no headlines in %s", argsName.get("path")));
+            }
+            Map<Integer, String> headsByNumbers = new HashMap<>();
+            String[] filter = argsName.get("filter").split(",");
+            String[] headlines = sc.nextLine().split(argsName.get("delimiter"));
+            for (String s : filter) {
+                for (int i = 0; i < headlines.length; i++) {
+                    if (headlines[i].equals(s)) {
+                        headsByNumbers.putIfAbsent(i, s);
+                    }
+                }
+            }
+            if (headsByNumbers.size() != filter.length) {
+                throw new NoSuchElementException(String.format("Not all colunms from %s are present in %s", argsName.get("filter"), argsName.get("path")));
+            }
+            if (!sc.hasNextLine()) {
+                throw new NoSuchElementException(String.format("There is no data in %s", argsName.get("path")));
+            }
+            List<ArrayList<String>> data = new ArrayList<>();
+            for (int i = 0; i < filter.length; i++) {
+                data.add(new ArrayList<>());
+            }
+            while (sc.hasNextLine()) {
+                String[] datas = sc.nextLine().split(argsName.get("delimiter"));
+                int i = 0;
+                for (Integer key : headsByNumbers.keySet()) {
+                    data.get(i++).add(datas[key]);
+                }
+            }
+
+            PrintStream outStream = (argsName.get("out").equals("stdout")) ? System.out : new PrintStream(new FileOutputStream(argsName.get("out")));
+            for (int i = 0; i < filter.length; i++) {
+                outStream.printf("%s", filter[i]);
+                if (i < filter.length - 1) {
+                    outStream.print(";");
+                }
+            }
+            outStream.print(System.lineSeparator());
+            for (int j = 0; j < data.get(0).size(); j++) {
+                for (int k = 0; k < data.size(); k++) {
+                    outStream.printf("%s", data.get(k).get(j));
+                    if (k < data.size() - 1) {
+                        outStream.print(";");
+                    }
+                }
+                outStream.print(System.lineSeparator());
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 4) {
+            throw new IllegalArgumentException("There must be four arguments");
+        }
+        ArgsName argsName = ArgsName.of(args);
+        validate(argsName);
+        handle(argsName);
+    }
+
+    private static void validate(ArgsName argsName) {
+        if (!argsName.get("path").toLowerCase().endsWith(".csv")) {
+            throw new IllegalArgumentException(String.format("Argument path must ends with .csv: %s", argsName.get("path")));
+        }
+        File fileInPath = new File(argsName.get("path"));
+        if (!(fileInPath.exists() || fileInPath.isFile())) {
+            throw new IllegalArgumentException(String.format("File %s not found or is not a file.", argsName.get("path")));
+        }
+        argsName.get("delimiter");
+        argsName.get("out");
+        argsName.get("filter");
+    }
+}
